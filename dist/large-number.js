7d2afbf59a18bec6ec71b644963ede8570cf972c
Alex San <sanpc@qq.com>
2021-10-10 13:03:47 +0800

    fix: entry

--- dist/large-number.js
+++ dist/large-number.js
@@ -9,153 +9,41 @@
 		root["largeNumber"] = factory();
 })(self, function() {
 return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ([
-/* 0 */
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-/* eslint-disable global-require */
-if (process.env.NODE_ENV === 'production') {
-  module.exports = __webpack_require__(1);
-} else {
-  module.exports = __webpack_require__(2);
-}
-
-
-/***/ }),
+/* 0 */,
 /* 1 */
-/***/ ((module) => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(true)
-		module.exports = factory();
-	else {}
-})(self, function() {
-return /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ([
-/* 0 */
-/***/ ((module, __unused_webpack_exports, __nested_webpack_require_502__) => {
-
-/* eslint-disable global-require */
-if (process.env.NODE_ENV === 'production') {
-  module.exports = __nested_webpack_require_502__(1);
-} else {
-  module.exports = __nested_webpack_require_502__(2);
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+function add(_a, _b) {
+  const maxLen = Math.max(_a.length, _b.length);
+  const a = _a.padStart(maxLen, '0');
+  const b = _b.padStart(maxLen, '0');
+  const result = ''.padStart(maxLen, '0').split('');
+  let i = maxLen;
+  let x = 0;
+  while (i > 0) {
+    const aa = +a[i - 1];
+    const bb = +b[i - 1];
+    const sum = aa + bb + x;
+    const j = sum % 10;
+    result[i - 1] = j;
+    x = sum >= 10 ? 1 : 0;
+    // eslint-disable-next-line no-plusplus
+    i--;
+  }
+  if (x) {
+    result.shift(x);
+  }
+  console.log(result.join(''));
 }
 
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (add);
 
-/***/ }),
-/* 1 */
-/***/ ((module) => {
-
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(true)
-		module.exports = factory();
-	else {}
-})(self, function() {
-return /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ([
-/* 0 */
-/***/ ((module) => {
-
-/* eslint-disable global-require */
-if (process.env.NODE_ENV === 'production') {
-  module.exports = Object(function webpackMissingModule() { var e = new Error("Cannot find module '../dist/large-number'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
-} else {
-  module.exports = Object(function webpackMissingModule() { var e = new Error("Cannot find module '../dist/large-number.min'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
-}
-
-
-/***/ })
-/******/ 	]);
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __nested_webpack_require_1144__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __nested_webpack_require_1144__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module used 'module' so it can't be inlined
-/******/ 	var __webpack_exports__ = __nested_webpack_require_1144__(0);
-/******/ 	__webpack_exports__ = __webpack_exports__["default"];
-/******/ 	
-/******/ 	return __webpack_exports__;
-/******/ })()
-;
-});
-
-/***/ }),
-/* 2 */
-/***/ ((module) => {
-
-!function(e,o){  true?module.exports=o():0}(self,(function(){return e=[e=>{"production"===process.env.NODE_ENV?e.exports=Object(function(){var e=new Error("Cannot find module '../dist/large-number'");throw e.code="MODULE_NOT_FOUND",e}()):e.exports=Object(function(){var e=new Error("Cannot find module '../dist/large-number.min'");throw e.code="MODULE_NOT_FOUND",e}())}],o={},r=function r(t){var n=o[t];if(void 0!==n)return n.exports;var u=o[t]={exports:{}};return e[t](u,u.exports,r),u.exports}(0),r=r.default;var e,o,r}));
-
-/***/ })
-/******/ 	]);
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __nested_webpack_require_3644__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __nested_webpack_require_3644__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module used 'module' so it can't be inlined
-/******/ 	var __webpack_exports__ = __nested_webpack_require_3644__(0);
-/******/ 	__webpack_exports__ = __webpack_exports__["default"];
-/******/ 	
-/******/ 	return __webpack_exports__;
-/******/ })()
-;
-});
-
-/***/ }),
-/* 2 */
-/***/ ((module) => {
-
-!function(r,e){ true?module.exports=e():0}(self,(function(){return r=[(r,e,o)=>{"production"===process.env.NODE_ENV?r.exports=o(1):r.exports=o(2)},r=>{var e;self,e=function(){return(()=>{var r=[r=>{"production"===process.env.NODE_ENV?r.exports=Object(function(){var r=new Error("Cannot find module '../dist/large-number'");throw r.code="MODULE_NOT_FOUND",r}()):r.exports=Object(function(){var r=new Error("Cannot find module '../dist/large-number.min'");throw r.code="MODULE_NOT_FOUND",r}())}],e={},o=function o(t){var n=e[t];if(void 0!==n)return n.exports;var u=e[t]={exports:{}};return r[t](u,u.exports,o),u.exports}(0);return o=o.default})()},r.exports=e()},r=>{var e,o;self,r.exports=(e=[r=>{"production"===process.env.NODE_ENV?r.exports=Object(function(){var r=new Error("Cannot find module '../dist/large-number'");throw r.code="MODULE_NOT_FOUND",r}()):r.exports=Object(function(){var r=new Error("Cannot find module '../dist/large-number.min'");throw r.code="MODULE_NOT_FOUND",r}())}],o={},function r(t){var n=o[t];if(void 0!==n)return n.exports;var u=o[t]={exports:{}};return e[t](u,u.exports,r),u.exports}(0).default)}],e={},o=function o(t){var n=e[t];if(void 0!==n)return n.exports;var u=e[t]={exports:{}};return r[t](u,u.exports,o),u.exports}(0),o=o.default;var r,e,o}));
 
 /***/ })
 /******/ 	]);
@@ -185,13 +73,50 @@ if (process.env.NODE_ENV === 'production') {
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
 /******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module used 'module' so it can't be inlined
-/******/ 	var __webpack_exports__ = __webpack_require__(0);
-/******/ 	__webpack_exports__ = __webpack_exports__["default"];
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _addBigNum__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_addBigNum__WEBPACK_IMPORTED_MODULE_0__["default"]);
+
+})();
+
+__webpack_exports__ = __webpack_exports__["default"];
 /******/ 	return __webpack_exports__;
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-/* eslint-disable global-require */
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('../dist/large-number');
-} else {
-  module.exports = require('../dist/large-number.min');
-}
+import addBigNum from './addBigNum';
+
+export default addBigNum;
